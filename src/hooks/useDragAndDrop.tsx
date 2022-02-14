9c17b77bbc571f46921caf7fa1004159c1dad6e7
최현준 <chj878194@woowahan.com>
2022-02-14 18:46:08 +0900

    refactor(state): make hook `drag-and-drop`

---
+++ src/hooks/useDragAndDrop.tsx
@@ -0,0 +1,94 @@
+import { useState, MouseEventHandler, useEffect } from 'react';
+
+interface ClickState {
+  clickStartTime: number;
+  isClicked: boolean;
+  r: number;
+  color: string;
+}
+
+const useDragAndDropDraw = (onMouseUp: MouseEventHandler) => {
+  const [cx, setX] = useState(-100);
+  const [cy, setY] = useState(-100);
+  const [clickCircle, setClickCircle] = useState<ClickState>({
+    clickStartTime: 0,
+    isClicked: false,
+    r: 0,
+    color: '#FFFFFF',
+  });
+  const [timerId, setTimerId] = useState(-1);
+
+  const genRandomColor = () => {
+    const randomNumber = Math.floor(Math.random() * 0xffffff);
+    const randColor = randomNumber.toString(16).padStart(6, '0');
+    return `#${randColor.toUpperCase()}`;
+  };
+
+  const getRadius = (nowStarted?: boolean) => {
+    const radiusBase = 10;
+    if (nowStarted) {
+      return radiusBase;
+    }
+    return radiusBase + (Date.now() - clickCircle.clickStartTime) / 5;
+  };
+
+  const whileMouseDownHandler = () => {
+    const r = getRadius();
+    setClickCircle({
+      ...clickCircle,
+      r,
+    });
+  };
+
+  useEffect(() => {
+    if (clickCircle.isClicked) {
+      const id = window.setInterval(() => {
+        whileMouseDownHandler();
+      }, 10);
+      setTimerId(id);
+    } else {
+      clearTimeout(timerId);
+      setTimerId(-1);
+    }
+  }, [clickCircle.isClicked]);
+
+  const mouseDownHandler: MouseEventHandler = (e) => {
+    const r = getRadius(true);
+    setClickCircle({
+      isClicked: true,
+      clickStartTime: Date.now(),
+      r,
+      color: genRandomColor(),
+    });
+    setX(e.clientX);
+    setY(e.clientY);
+  };
+
+  const mouseMoveHandler: MouseEventHandler = (e) => {
+    if (clickCircle.isClicked) {
+      setX(e.clientX);
+      setY(e.clientY);
+    }
+  };
+
+  const mouseUpHandler: MouseEventHandler = (e) => {
+    onMouseUp(e);
+    setClickCircle({
+      isClicked: false,
+      clickStartTime: 0,
+      r: 0,
+      color: '#FFFFFF',
+    });
+    setX(-100);
+    setY(-100);
+  };
+
+  return {
+    nowSelectedCircle: { x: cx, y: cy, metadata: { ...clickCircle } },
+    mouseDownHandler,
+    mouseMoveHandler,
+    mouseUpHandler,
+  };
+};
+
+export default useDragAndDropDraw;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,24 +1,12 @@
-import React, {
-  MouseEventHandler,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
+import React, { useMemo, useState } from 'react';
 import { nanoid } from 'nanoid';
 import { useTranslation } from 'react-i18next';
 import * as S from 'components/Container.style';
+import useDragAndDropDraw from 'hooks/useDragAndDrop';
 
 interface CircleState {
   x: number;
   y: number;
-  r: number;
-  color: string;
-}
-
-interface ClickState {
-  clickStartTime: number;
-  isClicked: boolean;
   r: number;
   color: string;
 }
@@ -44,22 +32,7 @@ const blendMode = [
 
 const MainPage: React.FC = () => {
   const [circleStates, setCircleStates] = useState<CircleState[]>([]);
-  const [cx, setX] = useState(-100);
-  const [cy, setY] = useState(-100);
-  const [clickCircle, setClickCircle] = useState<ClickState>({
-    clickStartTime: 0,
-    isClicked: false,
-    r: 0,
-    color: '#FFFFFF',
-  });
-  const [timerId, setTimerId] = useState(-1);
   const [blendIndex, setBlendIndex] = useState(0);
-
-  const genRandomColor = useCallback(() => {
-    const randomNumber = Math.floor(Math.random() * 0xffffff);
-    const randColor = randomNumber.toString(16).padStart(6, '0');
-    return `#${randColor.toUpperCase()}`;
-  }, []);
 
   const genCircle = (x: number, y: number, r: number, color: string) => {
     setCircleStates([
@@ -73,64 +46,18 @@ const MainPage: React.FC = () => {
     ]);
   };
 
-  const getRadius = (nowStarted?: boolean) => {
-    const radiusBase = 10;
-    if (nowStarted) {
-      return radiusBase;
-    }
-    return radiusBase + (Date.now() - clickCircle.clickStartTime) / 5;
-  };
-
-  const whileMouseDownHandler = () => {
-    const r = getRadius();
-    setClickCircle({
-      ...clickCircle,
-      r,
-    });
-  };
-
-  useEffect(() => {
-    if (clickCircle.isClicked) {
-      const id = window.setInterval(() => {
-        whileMouseDownHandler();
-      }, 10);
-      setTimerId(id);
-    } else {
-      clearTimeout(timerId);
-      setTimerId(-1);
-    }
-  }, [clickCircle.isClicked]);
-
-  const mouseDownHandler: MouseEventHandler = (e) => {
-    const r = getRadius(true);
-    setClickCircle({
-      isClicked: true,
-      clickStartTime: Date.now(),
-      r,
-      color: genRandomColor(),
-    });
-    setX(e.clientX);
-    setY(e.clientY);
-  };
-
-  const mouseMoveHandler: MouseEventHandler = (e) => {
-    if (clickCircle.isClicked) {
-      setX(e.clientX);
-      setY(e.clientY);
-    }
-  };
-
-  const mouseUpHandler: MouseEventHandler = (e) => {
-    genCircle(e.clientX, e.clientY, getRadius(), clickCircle.color);
-    setClickCircle({
-      isClicked: false,
-      clickStartTime: 0,
-      r: 0,
-      color: '#FFFFFF',
-    });
-    setX(-100);
-    setY(-100);
-  };
+  const {
+    nowSelectedCircle: {
+      x,
+      y,
+      metadata: { r, color, isClicked },
+    },
+    mouseDownHandler,
+    mouseMoveHandler,
+    mouseUpHandler,
+  } = useDragAndDropDraw((e) => {
+    genCircle(e.clientX, e.clientY, r, color);
+  });
 
   const circles = useMemo(
     () =>
@@ -149,9 +76,7 @@ const MainPage: React.FC = () => {
     >
       <S.Header>{t('header')}</S.Header>
       {circles}
-      {clickCircle.isClicked && (
-        <S.Circle x={cx} y={cy} r={clickCircle.r} color={clickCircle.color} />
-      )}
+      {isClicked && <S.Circle x={x} y={y} r={r} color={color} />}
       <S.Button
         onMouseDown={(e) => {
           e.stopPropagation();
